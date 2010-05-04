ea86cb3f1087a2e1b0b8b4df4ac6b7fa9023d4d3
Raymond van Dongelen <dongelen@nhl.nl>
2010-05-04 09:29:50 +0200

    Removed spelling error

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,8 +1,7 @@
 require "imagize"
 class ExampleController < ApplicationController      
   def images     
-    imagizer = Imagize::Imagizer.new            
-    puts params[:tweet]
+    imagizer = Imagize::Imagizer.new
     render :partial=>"image", :collection => imagizer.imagize(params[:tweet])
   end
 end
